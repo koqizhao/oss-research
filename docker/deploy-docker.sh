1d4c481ca5903424b39946ff5b34a188acc16b82
Qiang Zhao <koqizhao@outlook.com>
2019-05-07 20:32:39 +0800

    refine docker deploy script

--- docker/deploy-docker.sh
+++ docker/deploy-docker.sh
@@ -6,14 +6,25 @@ echo
 
 source ~/Research/servers.sh
 
+prerequisites="curl apt-transport-https ca-certificates curl gnupg-agent software-properties-common"
+#mirror_site=https://download.docker.com/linux/ubuntu
+mirror_site=https://mirrors.aliyun.com/docker-ce/linux/ubuntu
+docker_packages="docker-ce docker-ce-cli containerd.io"
+
 deploy()
 {
-    ssh $1 "echo '$PASSWORD' | sudo -S apt-key add /media/sf_share/docker/gpg"
-    ssh $1 "echo '$PASSWORD' | sudo -S add-apt-repository \"deb [arch=amd64] https://mirrors.aliyun.com/docker-ce/linux/ubuntu \$(lsb_release -cs) stable\""
+    echo "deploy $1 started"
+
+    ssh $1 "echo '$PASSWORD' | sudo -S apt install $prerequisites"
+    ssh $1 "curl -fsSL https://download.docker.com/linux/ubuntu/gpg > gpg"
+    ssh $1 "echo '$PASSWORD' | sudo -S apt-key add gpg; rm gpg"
+    ssh $1 "echo '$PASSWORD' | sudo -S add-apt-repository \"deb [arch=amd64] $mirror_site \$(lsb_release -cs) stable\""
     ssh $1 "echo '$PASSWORD' | sudo -S apt update"
-    ssh $1 "echo '$PASSWORD' | sudo -S apt install -y docker-ce"
+    ssh $1 "echo '$PASSWORD' | sudo -S apt install -y $docker_packages"
     ssh $1 "echo '$PASSWORD' | sudo -S apt upgrade -y"
     echo
+
+    echo "deploy $1 finished"
 }
 
 for i in "${servers[@]}"
@@ -21,4 +32,3 @@ do
     deploy $i
     echo
 done
-
