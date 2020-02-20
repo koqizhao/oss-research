8ef91ab73abec5de3f94d9647c3ce9832bbe9bf4
koqizhao <koqizhao@outlook.com>
2020-02-20 20:28:40 +0800

    add more k8s files

--- kubernetes/init-env.sh
+++ kubernetes/init-env.sh
@@ -10,10 +10,18 @@ update-alternatives --set ip6tables /usr/sbin/ip6tables-legacy
 update-alternatives --set arptables /usr/sbin/arptables-legacy
 update-alternatives --set ebtables /usr/sbin/ebtables-legacy
 
+iptables -A INPUT -p tcp --dport 53 -j ACCEPT
+iptables -A INPUT -p udp --dport 53 -j ACCEPT
 iptables -A INPUT -p tcp --dport 6443 -j ACCEPT
 iptables -A INPUT -p tcp --dport 2379:2380 -j ACCEPT
 iptables -A INPUT -p tcp --dport 10250:10252 -j ACCEPT
 iptables -A INPUT -p tcp --dport 30000:32767 -j ACCEPT
+
+# for: kubectl expose NodePort service
+iptables -P FORWARD ACCEPT
+
+# for: kubectl proxy
+iptables -A INPUT -p tcp --dport 8001 -j ACCEPT
 
 # for other common use
 iptables -A INPUT -p tcp --dport 22 -j ACCEPT
@@ -22,8 +30,11 @@ iptables -A INPUT -p tcp --dport 8080 -j ACCEPT
 iptables -A INPUT -p tcp --dport 9000 -j ACCEPT
 iptables -A INPUT -p tcp --dport 9092 -j ACCEPT
 iptables -A INPUT -p tcp --dport 9200 -j ACCEPT
-iptables -A INPUT -p tcp --dport 3000 -j ACCEPT
 iptables -A INPUT -p tcp --dport 3306 -j ACCEPT
+iptables -A INPUT -p tcp --dport 3000 -j ACCEPT
 iptables -A INPUT -p tcp --dport 5601 -j ACCEPT
+
+# for all ports
+#iptables -A INPUT -p tcp --dport 0:65535 -j ACCEPT
 
 netfilter-persistent save
