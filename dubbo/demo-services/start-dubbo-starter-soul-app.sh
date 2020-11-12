67a0d68936238fbde5234b6ea3a7c9f40cf40d60
koqizhao <koqizhao@outlook.com>
2020-11-12 20:53:46 +0800

    dubbo/demo-services: add dubbo registry timeout

--- dubbo/demo-services/start-dubbo-starter-soul-app.sh
+++ dubbo/demo-services/start-dubbo-starter-soul-app.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
 java -Ddubbo.config-center.timeout=10000 \
+    -Ddubbo.registry.timeout=10000 \
     -jar io.study.dubbo-starter-soul-app-0.0.1.jar > soul-app.out 2>&1 &
--- dubbo/demo-services/start-dubbo-starter-springboot-client.sh
+++ dubbo/demo-services/start-dubbo-starter-springboot-client.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
 java -Ddubbo.config-center.timeout=10000 \
+    -Ddubbo.registry.timeout=10000 \
     -jar io.study.dubbo-starter-springboot-client-0.0.1.jar > springboot-client.out 2>&1 &
