d0d254195f2bb96a5dcf199e6b439022f996f6ea
koqizhao <koqizhao@outlook.com>
2020-11-14 02:43:11 +0800

    zkui: use port 2190

--- middleware/monitoring/elk/servers-dist.sh
+++ middleware/monitoring/elk/servers-dist.sh
@@ -6,4 +6,4 @@ es_servers_map=([192.168.56.11]=server1 \
 
 kibana_servers=(192.168.56.11)
 
-filebeat_servers=(192.168.56.11 192.168.56.12 192.168.56.13)
+filebeat_servers=(192.168.56.11 192.168.56.12)
