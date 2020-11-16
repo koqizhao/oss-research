ba6a7b7f3cd3c8b764c3850ecea64e85b1f46362
koqizhao <koqizhao@outlook.com>
2020-11-16 21:27:22 +0800

    monitoring/metric/prometheus: add alert rule

--- middleware/monitoring/metric/prometheus/deploy-service.sh
+++ middleware/monitoring/metric/prometheus/deploy-service.sh
@@ -7,6 +7,16 @@ source common.sh
 
 deploy_file=prometheus-2.22.1.linux-amd64
 
+ams=""
+for am in ${alertmanager_servers[@]}
+do
+    if [ -z "$ams" ]; then
+        ams="'$am:9093'"
+    else
+        ams="$ams, '$am:9093'"
+    fi
+done
+
 remote_deploy()
 {
     server=$1
@@ -18,10 +28,13 @@ remote_deploy()
     ssh $server "cd $deploy_path; tar xf ${deploy_file}.tar.gz; mv $deploy_file $component; rm ${deploy_file}.tar.gz"
 
     sed "s/THANOS_REPLICA/$server/g" prometheus.yml \
+        | sed "s/ALERT_MANAGERS/$ams/g" \
         > prometheus.yml.tmp
     scp prometheus.yml.tmp $server:$deploy_path/$component/prometheus.yml
     rm prometheus.yml.tmp
 
+    ssh $server "mkdir $deploy_path/$component/rules"
+    scp rules/* $server:$deploy_path/$component/rules
     scp start.sh $server:$deploy_path/$component
 
     scp prometheus.service $server:$deploy_path
