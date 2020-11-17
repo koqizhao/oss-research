2dbbd79f25e09b433eeab1eb63a2c008bb4ef625
koqizhao <koqizhao@outlook.com>
2020-11-17 11:15:36 +0800

    monitoring/metric: add more sd configs

--- middleware/monitoring/metric/prometheus/deploy-service.sh
+++ middleware/monitoring/metric/prometheus/deploy-service.sh
@@ -7,15 +7,41 @@ source common.sh
 
 deploy_file=prometheus-2.22.1.linux-amd64
 
-ams=""
-for am in ${alertmanager_servers[@]}
-do
-    if [ -z "$ams" ]; then
-        ams="'$am:9093'"
-    else
-        ams="$ams, '$am:9093'"
-    fi
-done
+generate_targets_config()
+{
+    declare tc=""
+    for s in ${target_servers[@]}
+    do
+        if [ -z "$tc" ]; then
+            tc="'$s:$target_port'"
+        else
+            tc="$tc, '$s:$target_port'"
+        fi
+    done
+    echo $tc
+}
+
+target_servers=${alertmanager_servers[@]}
+target_port=9093
+amtc="`generate_targets_config`"
+target_servers=${prometheus_servers[@]}
+target_port=9090
+ptc="`generate_targets_config`"
+target_servers=${grafana_servers[@]}
+target_port=3000
+gtc="`generate_targets_config`"
+target_servers=${node_exporter_servers[@]}
+target_port=9100
+ntc="`generate_targets_config`"
+target_servers=${pushgateway_servers[@]}
+target_port=9091
+pgtc="`generate_targets_config`"
+sed "s/TARGETS_ALERTMANAGER/$amtc/g" prometheus.yml \
+    | sed "s/TARGETS_PROMETHEUS/$ptc/g" \
+    | sed "s/TARGETS_GRAFANA/$gtc/g" \
+    | sed "s/TARGETS_NODE_EXPORTER/$ntc/g" \
+    | sed "s/TARGETS_PUSHGATEWAY/$pgtc/g" \
+    > prometheus.yml.tmp
 
 remote_deploy()
 {
@@ -27,14 +53,17 @@ remote_deploy()
     scp ~/Software/metric/prometheus/${deploy_file}.tar.gz $server:$deploy_path
     ssh $server "cd $deploy_path; tar xf ${deploy_file}.tar.gz; mv $deploy_file $component; rm ${deploy_file}.tar.gz"
 
-    sed "s/THANOS_REPLICA/$server/g" prometheus.yml \
-        | sed "s/ALERT_MANAGERS/$ams/g" \
-        > prometheus.yml.tmp
-    scp prometheus.yml.tmp $server:$deploy_path/$component/prometheus.yml
-    rm prometheus.yml.tmp
+    sed "s/THANOS_REPLICA/$server/g" prometheus.yml.tmp \
+        > prometheus.yml.tmp2
+    scp prometheus.yml.tmp2 $server:$deploy_path/$component/prometheus.yml
+    rm prometheus.yml.tmp2
 
     ssh $server "mkdir $deploy_path/$component/rules"
     scp rules/* $server:$deploy_path/$component/rules
+
+    ssh $server "mkdir $deploy_path/$component/sd_configs"
+    scp sd_configs/* $server:$deploy_path/$component/sd_configs
+
     scp start.sh $server:$deploy_path/$component
 
     scp prometheus.service $server:$deploy_path
@@ -46,3 +75,5 @@ remote_deploy()
 }
 
 batch_deploy
+
+rm prometheus.yml.tmp
