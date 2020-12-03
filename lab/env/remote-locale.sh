0afc60c9d5c4c45dcd4f7e86f835bfa2febe5d82
koqizhao <koqizhao@outlook.com>
2020-12-03 21:58:51 +0800

    fix locale script

--- lab/env/remote-locale.sh
+++ lab/env/remote-locale.sh
@@ -10,8 +10,9 @@ source ~/Research/lab/deploy/remote.sh
 
 for i in ${servers[@]}
 do
-    echo "server: $i"
-    ssh $i "echo '$PASSWORD' | sudo -S update-locale LANG=en_US.UTF-8; \
+    echo "\nserver: $i\n"
+    ssh $i "echo '$PASSWORD' | sudo -S locale-gen zh_CN.utf8; \
+        echo '$PASSWORD' | sudo -S update-locale LANG=zh_CN.UTF-8; \
         echo '$PASSWORD' | sudo -S rm -f /etc/localtime;
         echo '$PASSWORD' | sudo -S ln -s /usr/share/zoneinfo/Asia/Shanghai /etc/localtime; "
     remote_reboot $i $PASSWORD
