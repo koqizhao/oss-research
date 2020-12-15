9eb01aedc80ce6190db6155231fc027f76f4fb11
koqizhao <koqizhao@outlook.com>
2020-12-16 03:53:24 +0800

    artemis: add host.ip config

--- middleware/sf/artemis/artemis/tomcat/start.sh
+++ middleware/sf/artemis/artemis/tomcat/start.sh
@@ -4,6 +4,6 @@ export JAVA_HOME=/usr/lib/jvm/java-8-openjdk-amd64
 
 export SERVER_IP=`ip addr | grep "global enp0s8" | awk '{ print $2 }' | awk -F '/' '{ print $1 }'`
 
-#export CATALINA_OPTS="$CATALINA_OPTS -Dhost.ip=$SERVER_IP -Djava.net.preferIPv4Stack=true"
+export CATALINA_OPTS="$CATALINA_OPTS -Dhost.ip=$SERVER_IP"
 
 bin/catalina.sh run
