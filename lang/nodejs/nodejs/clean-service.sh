8e349331d358492b29139cdcf6e3eb24e5526d76
koqizhao <koqizhao@outlook.com>
2022-04-28 11:02:39 +0800

    nodejs: use version 16.x

--- lang/nodejs/nodejs/clean-service.sh
+++ lang/nodejs/nodejs/clean-service.sh
@@ -9,6 +9,8 @@ remote_clean()
 {
     ssh $1 "echo '$PASSWORD' | sudo -S apt purge -y nodejs"
     ssh $1 "echo '$PASSWORD' | sudo -S apt autoremove -y --purge"
+    ssh $1 "echo '$PASSWORD' | sudo -S rm -f /etc/apt/sources.list.d/nodesource.list"
+    ssh $1 "echo '$PASSWORD' | sudo -S apt update"
     ssh $1 "rm -f ~/.npmrc; rm -rf ~/.npm; "
 }
 
--- lang/nodejs/nodejs/deploy-service.sh
+++ lang/nodejs/nodejs/deploy-service.sh
@@ -5,16 +5,17 @@ init_scale "$1" ..
 
 source common.sh
 
-nodejs_version=18.x
+nodejs_version=16.x
 
 remote_deploy()
 {
     ssh $1 "curl -fsSL https://deb.nodesource.com/setup_$nodejs_version > setup_nodejs.sh; echo '$PASSWORD' | sudo -S /bin/bash ./setup_nodejs.sh; rm setup_nodejs.sh;"
     ssh $1 "echo '$PASSWORD' | sudo -S apt install -y nodejs"
+    ssh $1 "echo '$PASSWORD' | sudo npm install -g npm"
 
-    ssh $1 "npm config set registry https://registry.npm.taobao.org; \
+    ssh $1 "npm config set registry https://registry.npmmirror.com; \
         npm config get registry; \
-        echo sass_binary_site=https://npm.taobao.org/mirrors/node-sass/ >> ~/.npmrc; "
+        echo sass_binary_site=https://registry.npmmirror.com/binary.html?path=node-sass >> ~/.npmrc; "
 }
 
 batch_deploy
