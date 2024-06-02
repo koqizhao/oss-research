78713ea0c03e49be9a7af709d9984448321ab30d
koqizhao <koqizhao@outlook.com>
2024-06-02 09:37:49 +0800

    refine remote-update

--- lab/env/remote-update.sh
+++ lab/env/remote-update.sh
@@ -8,6 +8,11 @@ read_server_pass
 source ~/Research/lab/env/servers.sh
 source ~/Research/lab/deploy/remote.sh
 
+if [ "$1" != "" ]; then
+    remote_update $1 $PASSWORD
+    exit 0
+fi
+
 for i in ${servers[@]}
 do
     remote_update $i $PASSWORD
