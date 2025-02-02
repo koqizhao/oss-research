ffd227a3ed0fa5ccaa8880922d1f01ac25a4df2b
Qiang Zhao <koqizhao@outlook.com>
2025-02-02 23:18:54 +0800

    refine

--- devops/cloud/docker/getstartedlab/join-token-worker.sh
+++ devops/cloud/docker/getstartedlab/join-token-worker.sh
@@ -1,2 +1,2 @@
-docker swarm join --token SWMTKN-1-4hff2x08xf2hyn56zso5itpec22l46w5xi8bmte7it4efa1j34-5a28pct4gdof61rairb7m9an5 192.168.56.101:2377
+docker swarm join --token SWMTKN-1-4hff2x08xf2hyn56zso5itpec22l46w5xi8bmte7it4efa1j34-5a28pct4gdof61rairb7m9an5 192.168.56.11:2377
 
