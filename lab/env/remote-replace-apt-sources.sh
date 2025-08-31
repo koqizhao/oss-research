b01fc7add151305ce72a71e7c71d91bacdc4df7a
Qiang Zhao <koqizhao@outlook.com>
2025-08-31 19:14:55 +0800

    update apt source script

--- lab/env/remote-replace-apt-sources.sh
+++ lab/env/remote-replace-apt-sources.sh
@@ -9,10 +9,10 @@ source ~/Research/lab/env/servers.sh
 
 for i in ${servers[@]}
 do
-    ssh $i "echo '$PASSWORD' | sudo -S rm -f /home/koqizhao/sources.list"
-    scp ~/Research/sources.list $i:./
-    ssh $i "echo '$PASSWORD' | sudo -S chown root:root /home/koqizhao/sources.list"
-    ssh $i "echo '$PASSWORD' | sudo -S mv /home/koqizhao/sources.list /etc/apt/"
+    ssh $i "echo '$PASSWORD' | sudo -S rm -f /home/koqizhao/aliyun.sources"
+    scp ~/Research/lab/env/aliyun.sources $i:./
+    ssh $i "echo '$PASSWORD' | sudo -S chown root:root /home/koqizhao/aliyun.sources"
+    ssh $i "echo '$PASSWORD' | sudo -S mv /home/koqizhao/aliyun.sources /etc/apt/sources.list.d/"
     ssh $i "echo '$PASSWORD' | sudo -S apt update"
     echo
 done
